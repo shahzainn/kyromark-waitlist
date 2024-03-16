b401aaadef1297b273dfd37c8d87b1c03215b9ee
Shahzain Arshad <shahzainarshad06@gmail.com>
2024-03-16 17:34:10 +0000

    Fixed TS typing

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Index() {
         inputRef.current!.value = '';
       }
     } catch (error) {
-      console.error('Error inserting data:', error.message);
+      console.error('Error inserting data:');
     }
   };
 
